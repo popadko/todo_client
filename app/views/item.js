0074aab6b09f5c55c08f5d2e6c29a51a9e1b9b75
Oleg Popadko <oleg.popadko@me.com>
2013-12-08 14:58:27 +0200

    edit todo by double click

--- app/views/item.js
+++ app/views/item.js
@@ -13,13 +13,16 @@ define([
 
         events: {
             'click .toggle': 'toggleCompleted',
-            'click .destroy': 'clear'
+            'click .destroy': 'clear',
+            'dblclick label': 'edit',
+            'keypress .edit': 'updateOnEnter',
+            'blur .edit': 'close'
         },
 
         initialize: function () {
-            _.bindAll(this, 'render', 'toggleVisible', 'isHidden', 'toggleCompleted', 'clear', 'remove');
+            _.bindAll(this, 'render', 'toggleVisible', 'isHidden', 'toggleCompleted', 'clear', 'edit', 'close', 'updateOnEnter');
             this.model.on('change', this.render);
-            this.model.on('destroy', this.remove);
+            this.model.on('destroy', this.remove, this);
             this.model.on('visible', this.toggleVisible);
             this.model.on('change:completed', this.toggleVisible);
             this.render();
@@ -29,6 +32,7 @@ define([
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggleClass('completed', this.model.get('completed'));
             this.toggleVisible();
+            this.$input = this.$('.edit');
         },
 
         toggleVisible: function () {
@@ -45,6 +49,30 @@ define([
 
         toggleCompleted: function () {
             this.model.toggle();
+        },
+
+        edit: function () {
+            this.$el.addClass('editing');
+            this.$input.focus();
+        },
+
+        close: function () {
+            var trimmedValue = this.$input.val().trim();
+            this.$input.val(trimmedValue);
+
+            if (trimmedValue) {
+                this.model.save({ title: trimmedValue });
+            } else {
+                this.clear();
+            }
+
+            this.$el.removeClass('editing');
+        },
+
+        updateOnEnter: function (e) {
+            if (e.which === App.ENTER_KEY) {
+                this.close();
+            }
         },
 
         clear: function () {
